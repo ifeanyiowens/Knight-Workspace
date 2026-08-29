c9a722c4399e05a6bc8758d7d9926a8edcef6118
Owen Oparaku <owen@oparakusystems.com>
2026-08-29 06:20:27 +0000

    Fix serverless function to use ES module export syntax matching the project

--- api/submit-brief.js
+++ api/submit-brief.js
@@ -11,7 +11,7 @@ const TEAM_SIZE_LABELS = {
   '35+': '35+ Enterprise',
 };
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method !== 'POST') {
     res.setHeader('Allow', 'POST');
     return res.status(405).json({ error: 'Method not allowed' });
